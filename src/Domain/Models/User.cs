8e1aecb4d11c4083e25b6352e81a2a9dd0c7dae2
ZeRoX0X <97478111+ZeRoX0X@users.noreply.github.com>
2024-10-28 14:24:50 +0300

    feature(user): add User and Role Identity

---
+++ src/Domain/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models;
+
+public class User : IdentityUser<Guid>
+{
+	     public bool IsActive { get; set; } = true;
+	     public string? FirstName {  get; set; }
+	     public string? LastName { get; set; }
+
+          
+
+}
+
---
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Domain.Models;
+
+namespace Infrastructure.Data;
+
+	public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+		
+}
+
